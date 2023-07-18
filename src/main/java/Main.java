19194ecf52fa054a3cbc68e9e7b244e2791b0a0d
Andrii Kovsh <kobannn@gmail.com>
2023-07-18 07:50:16 +0300

    Homework with output is added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        Student groupLeader = new Student(1, "Василь", "Чірт");
+        StudentsGroup studentsGroup = new StudentsGroup(groupLeader);
+        Student student1 = new Student(2, "Петро", "Попадайло");
+        Student student2 = new Student(3, "Іра", "Дика");
+        Student student3 = new Student(4, "Алла", "Бараболя");
 
+        studentsGroup.addStudent(groupLeader);
+        studentsGroup.addStudent(student1);
+        studentsGroup.addStudent(student2);
+        studentsGroup.addStudent(student3);
+
+        studentsGroup.addTask("Не наткнутись на виключення в коді");
+        studentsGroup.addTask("Вивчити цикли: for, while, for each");
+
+        studentsGroup.displayGroupDetails();
+    }
 }
--- src/main/java/StudentsGroup.java
+++ src/main/java/StudentsGroup.java
@@ -1,3 +1,57 @@
-public class StudentsGroup {
+import java.util.ArrayList;
+import java.util.List;
 
+public class StudentsGroup {
+    private Student groupLeader;
+    private final List<Student> students;
+    private final List<String> tasks;
+
+    public StudentsGroup(Student groupLeader) {
+        this.groupLeader = null;
+        this.students = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+        setGroupLeader(groupLeader);
+    }
+
+    public void changeGroupLeader(Student newGroupLeader) {
+        setGroupLeader(newGroupLeader);
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+
+    public void addTask(String task) {
+        tasks.add(task);
+    }
+
+    public void markTaskAsCompleted(Student student, String task) {
+        // Логіка позначення завдання як виконаного для зазначеного студента
+    }
+
+    public boolean hasGroupLeader() {
+        return groupLeader != null;
+    }
+
+    private void setGroupLeader(Student newGroupLeader) {
+        if (newGroupLeader != null) {
+            this.groupLeader = newGroupLeader;
+        }
+    }
+
+    public void displayGroupDetails() {
+        System.out.println("Староста: " + groupLeader.getFirstName() + " " + groupLeader.getLastName());
+        System.out.println("Студенти:");
+        for (Student student : students) {
+            System.out.println("- " + student.getFirstName() + " " + student.getLastName() + " (ID: " + student.getId() + ")");
+        }
+        System.out.println("Задачі:");
+        for (String task : tasks) {
+            System.out.println("- " + task);
+        }
+    }
 }
